a971358142a0fe3adfc4922c14329de7be21818c
Elan Fraiman <elfraiman@gmail.com>
2018-05-17 12:55:40 +0200

    initial commit

---
+++ header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="./style.css"/>
+    <title></title>
+</head>
+<body>
+
+<header>
+    <nav>
+        <div class="main-wrapper">
+            <ul>
+                <li><a href="index.php">Home</a></li>
+            </ul>
+            <div class="nav-login"> 
+                <form>
+                    <input type="text" name="uid" placeholder="Username">
+                    <input type="password" name="pwd" placeholder="Password">
+                    <button type="submit" name="submit">Login</button>
+                </form>
+                <a href="signup.php">Signup</a>
+            </div>
+        </div>
+    </nav>
+</header>
